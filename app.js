94bd06d38f885245912c579019525c13fea0e6e7
Marcos Rene <renemoura789@gmail.com>
2024-04-04 19:28:28 -0300

    feat: add backgroundColor when item selected

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // Elementos do DOM
 const modalOverlay = document.querySelector('.modal-overlay');
 const productCardBtns = document.querySelectorAll('.product-card');
+const sizeItems = document.querySelectorAll('.size-list li');
 const closeModalBtn = document.getElementById('close-modal-button');
 const cancelBtn = document.getElementById('cancel-button');
 const decrementBtn = document.getElementById('decrement-button');
@@ -19,16 +20,33 @@ const handleToggleModal = () => modalOverlay.classList.toggle('show');
 function closeModalAndUpdateCart() {
   handleToggleModal();
   quantityInput.value = 0;
-};
+}
+
+// Função para lidar com a seleção de item
+function handleItemSelected() {
+  // Removendo a classe 'active' de todos os itens
+  sizeItems.forEach((item) => {
+    item.classList.remove('active');
+  });
+  // Adicionando a classe 'active' apenas ao item clicado
+  this.classList.add('active');
+}
+
+// Adicionando evento de clique a cada item da lista
+sizeItems.forEach((item) => {
+  item.addEventListener('click', handleItemSelected);
+});
 
 // Função para adicionar um item ao carrinho
 function addToCart() {
   shoppingCartBtn.classList.add('shopping-bag-icon');
   shoppingCartBtn.dataset.items = cartItems++;
-};
+}
 
 // Adicionando evento de clique aos botões do produto para abrir o modal
-productCardBtns.forEach((cardBtn) => cardBtn.addEventListener('click', handleToggleModal));
+productCardBtns.forEach((cardBtn) =>
+  cardBtn.addEventListener('click', handleToggleModal)
+);
 
 // Adicionando eventos de clique aos botões de fechar o modal
 closeModalBtn.addEventListener('click', closeModalAndUpdateCart);
@@ -46,13 +64,13 @@ function decrementQuantity() {
   if (currentValue > 0) {
     quantityInput.value = currentValue - 1;
   }
-};
+}
 
 // Função para incrementar a quantidade
 function incrementQuantity() {
   let currentValue = parseInt(quantityInput.value);
   quantityInput.value = currentValue + 1;
-};
+}
 
 // Adicionando eventos de clique aos botões de decremento e incremento
 decrementBtn.addEventListener('click', decrementQuantity);
